272dbad06f02bfe9470f300da8ad4e2d9ced9160
rodrigocitadin <citadinrodrigo@gmail.com>
2024-10-02 15:35:15 -0300

    last tests

--- calcs.tests/CaculatorTest.cs
+++ calcs.tests/CaculatorTest.cs
@@ -27,4 +27,42 @@ public class CalculatorTest
             Assert.That(1, Is.EqualTo(result.b));
         });
     }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    public void Rem_MustReturnZero(int value)
+    {
+        Calculator c = new Calculator();
+        var result = c.Rem(12, value);
+
+        Assert.That(0, Is.EqualTo(result.b));
+    }
+
+    [Test]
+    public void Sub_MustReturnCorrectAnswer()
+    {
+        Calculator c = new Calculator();
+        var result = c.Sub(20, 10);
+
+        Assert.That(10, Is.EqualTo(result));
+    }
+
+    [Test]
+    public void Div_MustReturnCorrectAnswer()
+    {
+        Calculator c = new Calculator();
+        var result = c.Div(100, 10);
+
+        Assert.That(10, Is.EqualTo(result));
+    }
+
+    [Test]
+    public void Mult_MustReturnCorrectAnswer()
+    {
+        Calculator c = new Calculator();
+        var result = c.Mult(5, 2);
+
+        Assert.That(10, Is.EqualTo(result));
+    }
 }
