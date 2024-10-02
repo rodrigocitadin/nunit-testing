24df92f9cefa95078d8b37519acdfc02ab81daa7
rodrigocitadin <citadinrodrigo@gmail.com>
2024-10-02 15:28:02 -0300

    calcs

---
+++ calcs/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace calcs;
+
+public class Calculator
+{
+    public int Sum(int x, int y) => x + y;
+    public int Sub(int x, int y) => x - y;
+    public int Mult(int x, int y) => x * y;
+    public int Div(int x, int y) => x / y;
+    public (int a, int b) Rem(int x, int y) => (x / y, x % y);
+}
+
